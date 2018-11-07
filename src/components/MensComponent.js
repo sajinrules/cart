18e859b097e308d7dde2125abe87156adc9cc750
sajina <sajina@qburst.com>
2018-11-07 17:51:23 +0530

    Mens container

--- src/components/MensComponent.js
+++ src/components/MensComponent.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 
 
@@ -8,9 +8,33 @@ class MensComponent extends React.Component {
   // }
 
   render() {
+    const width = {
+      width: '80%'
+    };
     return (
       <div className="content container">
         <h3>Mens</h3>
+        <div>
+          <div className="product">
+            <span className="offer">-20%</span>
+            <img className="product-image" src="https://image.made-in-china.com/2f0j00dsfQOZLEZbzo/The-Wholesale-Mens-Dress-Shirts-Models-with-Mens-Casual-Shirts-Men-Wash-and-Wear.jpg" />
+          </div>
+          <div>
+            <div class="ratings">
+              <div class="empty-stars"></div>
+              <div class="full-stars" style={width}></div>
+            </div>
+            <div>Brand Name</div>
+            <div>
+              <span className="new-price">$16.00</span>
+              <span className="old-price">$20.00</span>
+            </div>
+          </div>
+          
+        </div>
+        <div>
+
+        </div>
       </div>
 
     )
